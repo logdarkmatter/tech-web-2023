3daf66002d05a9fc3c19ee16508b7a7d05223de3
Fernando Muniz <fernando.munizz89@gmail.com>
2023-03-06 12:32:45 +0000

    fix: small fixes in animations

--- code/react/pokedex-workshop/src/components/Create.jsx
+++ code/react/pokedex-workshop/src/components/Create.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect, useMemo } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+
+import pokeball from "../assets/pokeball.png";
 import "../App.css";
 
 function Create() {
@@ -16,6 +20,17 @@ function Create() {
     pokemonColor: "",
   });
   const [validated, setValidated] = useState(false);
+
+  const hoverAnimation = {
+    scale: 1.1, // aumenta a escala do componente em 10%
+    rotate: [-10, 10, -10, 0, -20, 20, -20, 0], // valores de rotação para a animação
+    transition: { duration: 0.5 }, // duração da animação
+  };
+
+  const tapAnimation = {
+    scale: 0.9, // diminui a escala do componente em 10%
+    transition: { duration: 0.2 }, // duração da animação
+  };
 
   const { name, pokedexNumber, pokemonType, pokemonColor } = formData;
 
@@ -157,21 +172,51 @@ function Create() {
               name="pokemonType"
               value={pokemonType}
             >
-              <option className="form-select-option" value="">-- Select a type --</option>
-              <option className="form-select-option" value="normal">Normal</option>
-              <option className="form-select-option" value="fire">Fire</option>
-              <option className="form-select-option" value="water">Water</option>
-              <option className="form-select-option" value="grass">Grass</option>
-              <option className="form-select-option" value="electric">Electric</option>
-              <option className="form-select-option" value="ice">Ice</option>
-              <option className="form-select-option" value="fighting">Fighting</option>
-              <option className="form-select-option" value="poison">Poison</option>
-              <option className="form-select-option" value="flying">Flying</option>
-              <option className="form-select-option" value="bug">Bug</option>
-              <option className="form-select-option" value="psychic">Psychic</option>
-              <option className="form-select-option" value="ghost">Ghost</option>
-              <option className="form-select-option" value="dark">Dark</option>
-              <option className="form-select-option" value="dragon">Dragon</option>
+              <option className="form-select-option" value="">
+                -- Select a type --
+              </option>
+              <option className="form-select-option" value="normal">
+                Normal
+              </option>
+              <option className="form-select-option" value="fire">
+                Fire
+              </option>
+              <option className="form-select-option" value="water">
+                Water
+              </option>
+              <option className="form-select-option" value="grass">
+                Grass
+              </option>
+              <option className="form-select-option" value="electric">
+                Electric
+              </option>
+              <option className="form-select-option" value="ice">
+                Ice
+              </option>
+              <option className="form-select-option" value="fighting">
+                Fighting
+              </option>
+              <option className="form-select-option" value="poison">
+                Poison
+              </option>
+              <option className="form-select-option" value="flying">
+                Flying
+              </option>
+              <option className="form-select-option" value="bug">
+                Bug
+              </option>
+              <option className="form-select-option" value="psychic">
+                Psychic
+              </option>
+              <option className="form-select-option" value="ghost">
+                Ghost
+              </option>
+              <option className="form-select-option" value="dark">
+                Dark
+              </option>
+              <option className="form-select-option" value="dragon">
+                Dragon
+              </option>
             </Form.Select>
           </Form.Group>
         </Row>
@@ -189,16 +234,36 @@ function Create() {
               name="pokemonColor"
               value={pokemonColor}
             >
-              <option className="form-select-option" value="">-- Select a color --</option>
-              <option className="form-select-option" value="red">Red</option>
-              <option className="form-select-option" value="green">Green</option>
-              <option className="form-select-option" value="blue">Blue</option>
-              <option className="form-select-option" value="yellow">Yellow</option>
-              <option className="form-select-option" value="black">Black</option>
-              <option className="form-select-option" value="white">White</option>
-              <option className="form-select-option" value="purple">Purple</option>
-              <option className="form-select-option" value="orange">Orange</option>
-              <option className="form-select-option" value="pink">Pink</option>
+              <option className="form-select-option" value="">
+                -- Select a color --
+              </option>
+              <option className="form-select-option" value="red">
+                Red
+              </option>
+              <option className="form-select-option" value="green">
+                Green
+              </option>
+              <option className="form-select-option" value="blue">
+                Blue
+              </option>
+              <option className="form-select-option" value="yellow">
+                Yellow
+              </option>
+              <option className="form-select-option" value="black">
+                Black
+              </option>
+              <option className="form-select-option" value="white">
+                White
+              </option>
+              <option className="form-select-option" value="purple">
+                Purple
+              </option>
+              <option className="form-select-option" value="orange">
+                Orange
+              </option>
+              <option className="form-select-option" value="pink">
+                Pink
+              </option>
             </Form.Select>
           </Form.Group>
         </Row>
@@ -206,6 +271,22 @@ function Create() {
           <Button className="custom-button submit-button" type="submit">
             Submit form
           </Button>
+        </div>
+        <div style={{ marginTop: "100px" }}>
+          <Row>
+            <Col>
+              <motion.img
+                whileHover={hoverAnimation}
+                whileTap={tapAnimation}
+                src={pokeball}
+                alt="Pokeball"
+                className="Pokeball"
+                style={{
+                  marginTop: "20px",
+                }}
+              />
+            </Col>
+          </Row>
         </div>
       </Form>
     </>
